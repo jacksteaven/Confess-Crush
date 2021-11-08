82175dd8a49f2f49987b01d027ceff278ebbd06a
jacksteaven <93298734+jacksteaven@users.noreply.github.com>
2021-11-08 17:27:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
+    titleWeb: "Ê nhok con cute",
     introTitle: 'Ê nhok con',
     introDesc: `Trái đất vốn lạ thường
     Mà sao cậu cứ đi nhầm đường
